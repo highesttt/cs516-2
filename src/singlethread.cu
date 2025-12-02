411b645fc497a4007d569faeb4d12f33b6529057
highesttt <github@sk.network>
2025-12-01 20:14:58 -0600

    feat: 🎸 add initial .clang-format and .gitignore files; refactor CUDA source files for consistency

--- src/singlethread.cu
+++ src/singlethread.cu
@@ -1,42 +1,45 @@
 // singlethread.cu
+#include <cuda_runtime.h>
+
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <cuda_runtime.h>
 
 using namespace std;
 
 // --- SKELETON ERROR CHECKING ---
 #define CUDA_CHECK_ERROR
-#define CudaSafeCall( err ) __cudaSafeCall( err, __FILE__, __LINE__ )
-#define CudaCheckError()    __cudaCheckError( __FILE__, __LINE__ )
+#define CudaSafeCall(err) __cudaSafeCall(err, __FILE__, __LINE__)
+#define CudaCheckError() __cudaCheckError(__FILE__, __LINE__)
 
-inline void __cudaSafeCall( cudaError err, const char *file, const int line ) {
+inline void __cudaSafeCall(cudaError err, const char* file, const int line) {
 #ifdef CUDA_CHECK_ERROR
-    if ( cudaSuccess != err ) {
-        fprintf( stderr, "cudaSafeCall() failed at %s:%i : %s\n", file, line, cudaGetErrorString( err ) );
-        exit( -1 );
+    if (cudaSuccess != err) {
+        fprintf(stderr, "cudaSafeCall() failed at %s:%i : %s\n", file, line,
+                cudaGetErrorString(err));
+        exit(-1);
     }
 #endif
     return;
 }
 
-inline void __cudaCheckError( const char *file, const int line ) {
+inline void __cudaCheckError(const char* file, const int line) {
 #ifdef CUDA_CHECK_ERROR
     cudaError_t err = cudaGetLastError();
-    if ( cudaSuccess != err ) {
-        fprintf( stderr, "cudaCheckError() failed at %s:%i : %s\n", file, line, cudaGetErrorString( err ) );
-        exit( -1 );
+    if (cudaSuccess != err) {
+        fprintf(stderr, "cudaCheckError() failed at %s:%i : %s\n", file, line,
+                cudaGetErrorString(err));
+        exit(-1);
     }
 #endif
     return;
 }
 // --- END SKELETON ---
 
-int * makeRandArray( const int size, const int seed ) {
-    srand( seed );
-    int * array = new int[ size ];
-    for( int i = 0; i < size; i ++ ) {
+int* makeRandArray(const int size, const int seed) {
+    srand(seed);
+    int* array = new int[size];
+    for (int i = 0; i < size; i++) {
         array[i] = std::rand() % 1000000;
     }
     return array;
@@ -58,51 +61,65 @@ __global__ void singleThreadSort(int* data, int n) {
     }
 }
 
-int main( int argc, char* argv[] ) {
-    int * array;
+int main(int argc, char* argv[]) {
+    int* array;
     int size, seed;
     bool printSorted = false;
 
-    if( argc < 4 ){
-        std::cerr << "usage: " << argv[0] << " [size] [seed] [print 1/0]" << std::endl;
-        exit(-1 );
+    if (argc < 4) {
+        std::cerr << "usage: " << argv[0] << " [size] [seed] [print 1/0]"
+                  << std::endl;
+        exit(-1);
     }
 
-    { std::stringstream ss1( argv[1] ); ss1 >> size; }
-    { std::stringstream ss1( argv[2] ); ss1 >> seed; }
-    { std::stringstream ss1( argv[3] ); int sortPrint; ss1 >> sortPrint; if(sortPrint == 1) printSorted = true; }
+    {
+        std::stringstream ss1(argv[1]);
+        ss1 >> size;
+    }
+    {
+        std::stringstream ss1(argv[2]);
+        ss1 >> seed;
+    }
+    {
+        std::stringstream ss1(argv[3]);
+        int sortPrint;
+        ss1 >> sortPrint;
+        if (sortPrint == 1)
+            printSorted = true;
+    }
 
-    array = makeRandArray( size, seed );
+    array = makeRandArray(size, seed);
 
     cudaEvent_t startTotal, stopTotal;
     float timeTotal;
     cudaEventCreate(&startTotal);
     cudaEventCreate(&stopTotal);
-    cudaEventRecord( startTotal, 0 );
+    cudaEventRecord(startTotal, 0);
 
-    // --- YOUR CODE HERE ---
     int* d_array;
     CudaSafeCall(cudaMalloc(&d_array, size * sizeof(int)));
-    CudaSafeCall(cudaMemcpy(d_array, array, size * sizeof(int), cudaMemcpyHostToDevice));
+    CudaSafeCall(
+        cudaMemcpy(d_array, array, size * sizeof(int), cudaMemcpyHostToDevice));
 
     // Launch with 1 block, 1 thread
     singleThreadSort<<<1, 1>>>(d_array, size);
     CudaCheckError();
 
-    CudaSafeCall(cudaMemcpy(array, d_array, size * sizeof(int), cudaMemcpyDeviceToHost));
+    CudaSafeCall(
+        cudaMemcpy(array, d_array, size * sizeof(int), cudaMemcpyDeviceToHost));
     cudaFree(d_array);
-    // --- END YOUR CODE ---
 
-    cudaEventRecord( stopTotal, 0 );
-    cudaEventSynchronize( stopTotal );
-    cudaEventElapsedTime( &timeTotal, startTotal, stopTotal );
-    cudaEventDestroy( startTotal );
-    cudaEventDestroy( stopTotal );
+    cudaEventRecord(stopTotal, 0);
+    cudaEventSynchronize(stopTotal);
+    cudaEventElapsedTime(&timeTotal, startTotal, stopTotal);
+    cudaEventDestroy(startTotal);
+    cudaEventDestroy(stopTotal);
 
     std::cerr << "Total time in seconds: " << timeTotal / 1000.0 << std::endl;
 
-    if( printSorted ){
-        for(int i=0; i<size; i++) cout << array[i] << (i < size-1 ? ", " : "\n");
+    if (printSorted) {
+        for (int i = 0; i < size; i++)
+            cout << array[i] << (i < size - 1 ? ", " : "\n");
     }
     delete[] array;
 }
